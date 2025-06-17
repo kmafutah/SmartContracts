8e11bf398258a94a45c9ebc6ce00ea4d37e17714
Kudakwashe Mafutah <kmafutah@gmail.com>
2025-06-17 18:09:04 +0200

    ...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SmartContracts/', // <--- IMPORTANT: Replace with your actual repository name
+  base: '/', // <--- IMPORTANT: Replace with your actual repository name
 })
